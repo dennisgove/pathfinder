fa1af00be022e00bbf77a4d0db3377c0bf4f381c
Dennis Gove <dpgove@gmail.com>
2018-10-27 10:37:07 -0700

    Added comments for VS Code

--- src/pathfinder/Main.java
+++ src/pathfinder/Main.java
@@ -5,8 +5,10 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
 
     // Object: Get to the bottom-right corner of the game board
-    // To Start: right-click on Main.java and choose "Run as Java Application"
-    //           ^^^ generally the f5 key is a shortcut for this
+    // To Start:
+    //    VS Code: Click the Debug button directly above this method
+    //    Eclipse: right-click on Main.java and choose "Run as Java Application"
+    //             ^^^ generally the f5 key is a shortcut for this in both
 
     int width = 10;
     int height = 10;
