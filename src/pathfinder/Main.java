9d47ab0e33cdd627c8bf2a30e01d048ed564f4f7
Dennis Gove <dpgove@gmail.com>
2018-10-30 23:51:58 -0400

    Finds valid path to take. May not be the shortest

--- src/pathfinder/Main.java
+++ src/pathfinder/Main.java
@@ -1,5 +1,10 @@
 package pathfinder;
 
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
@@ -10,16 +15,66 @@ public class Main {
     //    Eclipse: right-click on Main.java and choose "Run as Java Application"
     //             ^^^ generally the f5 key is a shortcut for this in both
 
-    int width = 10;
+    int width = 40;
     int height = 10;
-    int numberRocks = 0;
+    int numberRocks = 150;
+
+    List<Move> movesToMake = new ArrayList<>();
 
     new GameBoard()
     .setMoveSelector((board, position) -> {
       // We need to decide the appropriate next move
 
-      return Move.RIGHT;
+      if(0 == movesToMake.size()){
+        movesToMake.addAll(findMoves(board, position, new HashSet<>()));
+      }
+
+      return movesToMake.remove(0);
     })
     .play(width, height, numberRocks);
+  }
+
+  private static List<Move> findMoves(GameBoard board, Position from, Set<Position> alreadyChecked){
+    if(alreadyChecked.contains(from)){
+      return null;
+    }
+
+    alreadyChecked.add(from);
+
+    switch(board.getSpotAt(from)){
+      case OFF_BOARD:
+      case ROCK:
+        return null;
+      case GOAL:
+        return new ArrayList<>();
+    }
+
+    List<Move> after;
+    
+    after = findMoves(board, from.right(), alreadyChecked);
+    if(null != after){
+      after.add(0, Move.RIGHT);
+      return after;
+    }
+
+    after = findMoves(board, from.down(), alreadyChecked);
+    if(null != after){
+      after.add(0, Move.DOWN);
+      return after;
+    }
+
+    after = findMoves(board, from.up(), alreadyChecked);
+    if(null != after){
+      after.add(0, Move.UP);
+      return after;
+    }
+
+    after = findMoves(board, from.left(), alreadyChecked);
+    if(null != after){
+      after.add(0, Move.LEFT);
+      return after;
+    }
+
+    return null;
   }
 }
